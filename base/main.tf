823f48d28ee02e6e9b972bdefc81786f558dc7b0
Jayakishore Roy <jayakishore@datastream.org>
2023-03-07 15:15:03 -0500

    feat: using launch template for autoscaling group

--- base/main.tf
+++ base/main.tf
@@ -1,40 +1,56 @@
-resource "aws_launch_configuration" "main" {
-  name_prefix          = "${local.name}-"
-  image_id             = local.image_id
-  instance_type        = var.instance_type
-  spot_price           = var.spot ? "0.0001" : null
-  key_name             = var.key_name
-  iam_instance_profile = aws_iam_instance_profile.main.name
+resource "aws_launch_template" "main" {
+  name                   = local.name
+  image_id               = local.image_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  ebs_optimized          = false
+  update_default_version = true
+  user_data              = base64encode(local.user_data)
 
-  security_groups = [
-    aws_security_group.main.id,
-  ]
+  dynamic "instance_market_options" {
+    for_each = var.spot ? [true] : []
+    content {
+      market_type = "spot"
 
-  user_data         = local.user_data
-  ebs_optimized     = "false"
-  enable_monitoring = "true"
+      spot_options {
+        spot_instance_type = "one-time"
+      }
+    }
+  }
+
+  dynamic "block_device_mappings" {
+    for_each = var.volume_size == 0 ? [] : [true]
+    content {
+      device_name = data.aws_ami.main.root_device_name
+
+      ebs {
+        volume_type           = var.volume_type
+        volume_size           = var.volume_size
+        delete_on_termination = true
+        encrypted             = true
+      }
+    }
+  }
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.main.arn
+  }
+
+  monitoring {
+    enabled = true
+  }
+
+  network_interfaces {
+    # Must be true in public subnets if assigning EIP in userdata
+    associate_public_ip_address = var.subnet_public
+    delete_on_termination       = true
+    security_groups             = [aws_security_group.main.id]
+  }
 
   metadata_options {
     http_endpoint               = "enabled"
     http_put_response_hop_limit = 1
     http_tokens                 = "required"
-  }
-  # Must be true in public subnets if assigning EIP in userdata
-  associate_public_ip_address = var.subnet_public
-
-  dynamic "root_block_device" {
-    for_each = var.volume_size == 0 ? [] : [
-      true]
-    content {
-      volume_type           = var.volume_type
-      volume_size           = var.volume_size
-      delete_on_termination = true
-      encrypted             = true
-    }
-  }
-
-  lifecycle {
-    create_before_destroy = true
   }
 }
 
@@ -44,9 +60,12 @@ resource "aws_autoscaling_group" "main" {
   min_size                  = local.min_size
   desired_capacity          = local.desired_capacity
   health_check_grace_period = 30
-  launch_configuration      = aws_launch_configuration.main.name
+  vpc_zone_identifier       = var.subnet_ids
 
-  vpc_zone_identifier = var.subnet_ids
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
 
   dynamic "tag" {
     for_each = local.tags
@@ -61,7 +80,7 @@ resource "aws_autoscaling_group" "main" {
 
 // Schedules
 resource "aws_autoscaling_schedule" "scale_up" {
-  count                  = var.schedule_scale_up_recurrence != "" ? 1: 0
+  count                  = var.schedule_scale_up_recurrence != "" ? 1 : 0
   autoscaling_group_name = aws_autoscaling_group.main.name
   scheduled_action_name  = "ScaleUp"
   min_size               = local.min_size
@@ -71,7 +90,7 @@ resource "aws_autoscaling_schedule" "scale_up" {
 }
 
 resource "aws_autoscaling_schedule" "scale_down" {
-  count                  = var.schedule_scale_down_recurrence != "" ? 1: 0
+  count                  = var.schedule_scale_down_recurrence != "" ? 1 : 0
   autoscaling_group_name = aws_autoscaling_group.main.name
   scheduled_action_name  = "ScaleDown"
   min_size               = local.min_size
@@ -81,7 +100,7 @@ resource "aws_autoscaling_schedule" "scale_down" {
 }
 
 resource "aws_autoscaling_schedule" "shut_down" {
-  count                  = var.schedule_shut_down_recurrence != "" ? 1: 0
+  count                  = var.schedule_shut_down_recurrence != "" ? 1 : 0
   autoscaling_group_name = aws_autoscaling_group.main.name
   scheduled_action_name  = "ShutDown"
   min_size               = 0
